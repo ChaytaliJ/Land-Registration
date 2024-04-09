2f451b12c55a24104b9ebb2d8dd15ac239574d93
Mandy767 <92856810+Mandy767@users.noreply.github.com>
2024-04-09 17:47:31 +0530

    changes

---
+++ client/src/Web3.tsx
@@ -0,0 +1,16 @@
+import Web3 from "web3";
+
+
+const getWeb3 = () =>
+    new Promise((resolve, reject) => {
+
+        const provider = new Web3.providers.HttpProvider(
+            "http://127.0.0.1:7545"
+        );
+        const web3 = new Web3(provider);
+        console.log("No web3 instance injected, using Local web3.");
+        resolve(web3);
+
+    });
+
+export default getWeb3;
