fb5138fc9516a723e0907821f236e383f8b7e8da
Mandy767 <92856810+Mandy767@users.noreply.github.com>
2024-04-11 17:20:43 +0530

    land registration

--- client/src/pages/components/land-card.tsx
+++ client/src/pages/components/land-card.tsx
@@ -6,7 +6,8 @@ import { Button } from "@/components/ui/button"
 import land from "../../assets/dimitri-karastelev-ZH4FUYiaczY-unsplash.jpg"
 import useContract from "@/hooks/useContract";
 import { useCallback, useEffect, useState } from "react";
-
+import { MdVerified } from "react-icons/md";
+import { GoUnverified } from "react-icons/go";
 //@ts-ignore
 export default function Landcard({ islandgallery, landId }) {
     const [loading, setLoading] = useState(true);
@@ -16,10 +17,22 @@ export default function Landcard({ islandgallery, landId }) {
     const getLandData = useCallback(async () => {
         try {
 
-            const landInfo = await contractInstance?.methods?.getLandInfo(landId).call();
+            const landDetails = await contractInstance?.methods?.getLandInfo(landId).call();
             const landVerification = await contractInstance?.methods?.isLandVerified(landId).call();
-
-            setLand(landInfo);
+            const details = {
+                index: parseInt(landDetails[0]),
+                area: landDetails[1].toString(),
+                state: landDetails[3],
+                price: parseInt(landDetails[4]),
+                pid: parseInt(landDetails[5]),
+                survey: parseInt(landDetails[6]),
+                document: landDetails[3].toString(),
+                ipfshash: landDetails[7].toString(),
+                owneraddress: landDetails[9],
+                verificationStatus: landVerification
+            }
+            console.log(details);
+            setLand(details);
             setVerification(landVerification)
             setLoading(false);
         } catch (error) {
@@ -38,14 +51,17 @@ export default function Landcard({ islandgallery, landId }) {
 
 
             <div className="pl-8">
-                <p>{landId}</p>
                 {
-                    verification ? (<p>verified</p>) : (<p>Not yet verified</p>)
+                    verification ? (<div className="flex flex-row space-x-2 "><MdVerified className="h-8 w-8" /><p className="font-extrabold text-2xl">Verified</p></div>) : (<div className="flex flex-row space-x-2 "><GoUnverified className="h-8 w-8" /><p className="font-extrabold text-2xl">Not Verified</p></div>)
                 }
 
-                <CardDescription>Size</CardDescription>
-                <CardDescription>Address</CardDescription>
-                <CardDescription>Price</CardDescription>
+                {
+                    islandgallery && (<p className="py-2 text-sm">{Land?.owneraddress}</p>)
+                }
+
+                <p className="font-bold text-lg" >{"State : " + Land?.state}</p>
+                <p className="font-bold text-lg" >{"Price : " + Land?.price + " ETH "}</p>
+
             </div>
             <div className="border-t w-full border-gray my-3"></div>
             <div className="flex flex-row items-center justify-center pb-4 space-x-10"> {/* Modified line */}
